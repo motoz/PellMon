213be77978e816e24a9bc8d8e0d6f92375382273
Peter Farsinsen <peter@farsinsen.dk>
2013-03-06 23:39:30 +0100

    Adding a autorefresh handler and fixing the client side logic accordingly

--- pellmonwebb.py
+++ pellmonwebb.py
@@ -38,30 +38,30 @@ parser = ConfigParser.ConfigParser()
 parser.read('pellmon.conf')
 
 # The RRD database, updated by pellMon
-db = parser.get('conf', 'database') 
+db = parser.get('conf', 'database')
 
 # And the colors to use when drawing the graph
 colors = parser.items('graphcolors')
 colorsDict = {}
 for key, value in colors:
     colorsDict[key] = value
-    
-# Get the names of the polled data 
+
+# Get the names of the polled data
 polldata = parser.items("pollvalues")
 
 timeChoices = ['time1h', 'time3h', 'time8h', 'time24h', 'time3d', 'time1w']
 timeNames  = ['1 hour', '3 hours', '8 hours', '24 hours', '3 days', '1 week']
-timeSeconds = [3600, 3600*3, 3600*8, 3600*24, 3600*24*3, 3600*24*7]    
+timeSeconds = [3600, 3600*3, 3600*8, 3600*24, 3600*24*3, 3600*24*7]
 
 
 class PellMonWebb:
 
     auth = AuthController()
-    
+
     @cherrypy.expose
     def form1(self, **args):
         # The checkboxes are submitted with 'post'
-        if cherrypy.request.method == "POST": 
+        if cherrypy.request.method == "POST":
             # put the selection in session
             for key,val in polldata:
                 # is this dataset checked?
@@ -69,8 +69,8 @@ class PellMonWebb:
                     # if so, set it in the session
                     cherrypy.session[val] = 'yes'
                 else:
-                    cherrypy.session[val] = 'no' 
-            
+                    cherrypy.session[val] = 'no'
+
             if args.has_key('autorefresh'):
                 cherrypy.session['autorefresh']='yes'
             else:
@@ -81,25 +81,36 @@ class PellMonWebb:
     @cherrypy.expose
     def form2(self, **args):
         # The radiobuttons are submitted with 'post'
-        if cherrypy.request.method == "POST": 
+        if cherrypy.request.method == "POST":
             if args.has_key('graphtime') and args.get('graphtime') in timeChoices:
                 cherrypy.session['timeChoice']=args.get('graphtime')
         # redirect back after setting selection in session
         raise cherrypy.HTTPRedirect('/')
-        
+
+
+    @cherrypy.expose
+    def autorefresh(self, **args):
+        if cherrypy.request.method == "POST":
+            if args.has_key('autorefresh') and args.get('autorefresh') == 'yes':
+                cherrypy.session['autorefresh'] = 'yes'
+            else:
+                cherrypy.session['autorefresh'] = 'no'
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return simplejson.dumps(dict(enabled=cherrypy.session['autorefresh']))
+
     @cherrypy.expose
     def left(self, **args):
         if not cherrypy.session.get('time'):
             cherrypy.session['time'] = '0'
         if not cherrypy.session.get('timeChoice'):
             cherrypy.session['timeChoice'] = 'time1h'
-        if cherrypy.request.method == "POST": 
+        if cherrypy.request.method == "POST":
             seconds=timeSeconds[timeChoices.index(cherrypy.session['timeChoice'])]
             cherrypy.session['time']=str(int(cherrypy.session['time'])+seconds)
         cherrypy.response.headers['Content-Type'] = 'application/json'
         return simplejson.dumps(dict(title="Hello"))
         # redirect back after setting selection in session
-        #raise cherrypy.HTTPRedirect('/')        
+        #raise cherrypy.HTTPRedirect('/')
 
     @cherrypy.expose
     def right(self, **args):
@@ -107,7 +118,7 @@ class PellMonWebb:
             cherrypy.session['time'] = '0'
         if not cherrypy.session.get('timeChoice'):
             cherrypy.session['timeChoice'] = 'time1h'
-        if cherrypy.request.method == "POST": 
+        if cherrypy.request.method == "POST":
             seconds=timeSeconds[timeChoices.index(cherrypy.session['timeChoice'])]
             time=int(cherrypy.session['time'])-seconds
             if time<0:
@@ -116,24 +127,24 @@ class PellMonWebb:
         cherrypy.response.headers['Content-Type'] = 'application/json'
         return simplejson.dumps(dict(title="Hello, %s" % name))
         # redirect back after setting selection in session
-        #raise cherrypy.HTTPRedirect('/')    
-                
+        #raise cherrypy.HTTPRedirect('/')
+
     @cherrypy.expose
     def image(self, **args):
         if not cherrypy.session.get('timeChoice'):
             cherrypy.session['timeChoice'] = 'time1h'
         if not cherrypy.session.get('time'):
-            cherrypy.session['time'] = '0'          
+            cherrypy.session['time'] = '0'
         graphTime = timeSeconds[timeChoices.index(cherrypy.session.get('timeChoice'))]
         offset = int(cherrypy.session['time'])
         graphTimeStart=str(graphTime + offset)
         graphTimeEnd=str(offset)
-        #Build the command string to make a graph from the database         
+        #Build the command string to make a graph from the database
         RrdGraphString1="rrdtool graph "+os.getcwd()+"/graph1.png --lower-limit 0 --right-axis 1:0 --width 700 --height 400 --end now-"+graphTimeEnd+"s --start now-"+graphTimeStart+"s "
         for key,value in polldata:
             if cherrypy.session.get(value)=='yes':
-                RrdGraphString1=RrdGraphString1+"DEF:%s="%key+db+":%s:AVERAGE LINE1:%s%s:\"%s\" "% (value, key, colorsDict[key], value) 
-        RrdGraphString1=RrdGraphString1+">>/dev/null"   
+                RrdGraphString1=RrdGraphString1+"DEF:%s="%key+db+":%s:AVERAGE LINE1:%s%s:\"%s\" "% (value, key, colorsDict[key], value)
+        RrdGraphString1=RrdGraphString1+">>/dev/null"
         os.system(RrdGraphString1)
         cherrypy.response.headers['Pragma'] = 'no-cache'
         return serve_file(os.getcwd()+'/graph1.png', content_type='image/png')
@@ -142,8 +153,8 @@ class PellMonWebb:
     @require() #requires valid login
     def getparam(self, param):
         parameterlist=getdb()
-        if cherrypy.request.method == "GET": 
-            if param in(parameterlist): 
+        if cherrypy.request.method == "GET":
+            if param in(parameterlist):
                 try:
                     result = getItem(param)
                 except:
@@ -156,8 +167,8 @@ class PellMonWebb:
     @require() #requires valid login
     def setparam(self, param, data=None):
         parameterlist=getdb()
-        if cherrypy.request.method == "POST": 
-            if param in(parameterlist): 
+        if cherrypy.request.method == "POST":
+            if param in(parameterlist):
                 try:
                     result = setItem(param, data)
                 except:
@@ -165,21 +176,21 @@ class PellMonWebb:
             else: result = 'not found'
             cherrypy.response.headers['Content-Type'] = 'application/json'
             return simplejson.dumps(dict(param=param, value=result))
-        
+
     @cherrypy.expose
     @require() #requires valid login
     def parameters(self, **args):
         paramQueue = Queue.Queue(300)
         ht = threading.Thread(target=parameterReader, args=(paramQueue,))
-        ht.start()    
-        # Get list of data/parameters 
+        ht.start()
+        # Get list of data/parameters
         parameterlist=getdb()
         values=['']*len(parameterlist)
         params={}
         for item in parameterlist:
             params[item] = ' '
             if args.has_key(item):
-                if cherrypy.request.method == "POST": 
+                if cherrypy.request.method == "POST":
                     # set parameter
                     try:
                         values[parameterlist.index(item)]=setItem(item, args[item][1])
@@ -188,14 +199,14 @@ class PellMonWebb:
                 else:
                     # get parameter
                     try:
-                        values[parameterlist.index(item)]=getItem(item)  
+                        values[parameterlist.index(item)]=getItem(item)
                     except:
                         values[parameterlist.index(item)]='error'
         paramQueue = Queue.Queue(300)
         # Set up a queue and start a thread to read all items to the queue, the parameter view will empty the queue bye calling /getparams/
         cherrypy.session['paramReaderQueue'] = paramQueue
         ht = threading.Thread(name='poll_thread', target=parameterReader, args=(paramQueue,))
-        ht.start()            
+        ht.start()
         tmpl = lookup.get_template("parameters.html")
         return tmpl.render(params=parameterlist, values=values, paramCount = len(parameterlist))
 
@@ -206,7 +217,7 @@ class PellMonWebb:
         parameterlist=getdb()
         paramReaderQueue = cherrypy.session.get('paramReaderQueue')
         params={}
-        if paramReaderQueue:   
+        if paramReaderQueue:
             while True:
                 try:
                     param,value = paramReaderQueue.get_nowait()
@@ -217,32 +228,32 @@ class PellMonWebb:
                     params[param]=value
                 except:
                     # queue is empty, send what's read so far
-                    break    
+                    break
             cherrypy.response.headers['Content-Type'] = 'application/json'
             return simplejson.dumps(params)
-    
+
     @cherrypy.expose
     def index(self, **args):
         if not cherrypy.session.get('timeChoice'):
-            cherrypy.session['timeChoice']=timeChoices[0]               
-        checkboxes=[] 
+            cherrypy.session['timeChoice']=timeChoices[0]
+        checkboxes=[]
         empty=True
-        for key, val in polldata:  
-            if cherrypy.session.get(val)=='yes':  
+        for key, val in polldata:
+            if cherrypy.session.get(val)=='yes':
                 checkboxes.append((val,True))
                 empty=False
             else:
                 checkboxes.append((val,''))
         autorefresh = cherrypy.session.get('autorefresh')=='yes'
         tmpl = lookup.get_template("index.html")
-        return tmpl.render(username=cherrypy.session.get('_cp_username'), checkboxes=checkboxes, empty=empty, autorefresh=autorefresh, timeChoices=timeChoices, timeNames=timeNames, timeChoice=cherrypy.session.get('timeChoice'))    
+        return tmpl.render(username=cherrypy.session.get('_cp_username'), checkboxes=checkboxes, empty=empty, autorefresh=autorefresh, timeChoices=timeChoices, timeNames=timeNames, timeChoice=cherrypy.session.get('timeChoice'))
 
 def parameterReader(q):
     parameterlist=getdb()
     for item in parameterlist:
         try:
             value = getItem(item)
-        except: 
+        except:
             value='error'
         q.put((item,value))
     q.put(('**end**','**end**'))
@@ -255,7 +266,7 @@ def setItem(item, value):
 
 def getdb():
     return notify.GetDB()
-    
+
 MEDIA_DIR = os.path.join(os.path.abspath("."), u"media")
 
 global_conf = {
@@ -272,15 +283,15 @@ global_conf = {
 app_conf =  {'/media':
                 {'tools.staticdir.on': True,
                  'tools.staticdir.dir': MEDIA_DIR}
-            }              
-             
+            }
+
 # Connect to pellmonsrv on the dbus system bus
 d = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
 notify = Gio.DBusProxy.new_sync(d, 0, None, 'org.pellmon.int', '/org/pellmon/int', 'org.pellmon.int', None)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-cherrypy.config.update(global_conf)                        
+cherrypy.config.update(global_conf)
 cherrypy.quickstart(PellMonWebb(), config=app_conf)
-                 
-                 
-                 
+
+
+
