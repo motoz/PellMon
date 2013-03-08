e92bd1986a2cec1fcae15064efb93530849502f6
Motoz <stokkheim@gmail.com>
2013-03-08 13:04:55 +0200

    efficient reversed log reading

--- logview.py
+++ logview.py
@@ -1,7 +1,9 @@
+import os
 import cherrypy
 import ConfigParser
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from itertools import islice
 
 #Look for temlates in this directory
 lookup = TemplateLookup(directories=['html'])
@@ -22,10 +24,30 @@ class LogViewer(object):
     @cherrypy.expose
     def getlines(self):    
         f = open(logfile, "r")
-        lines = f.readlines()
+        #lines = f.readlines()
+        lines = islice(reversed_lines(f), 100):        
         tmpl = lookup.get_template("loglines.html")
         return tmpl.render(lines=lines)
         
 parser = ConfigParser.ConfigParser()
 
+def reversed_lines(file):
+    "Generate the lines of file in reverse order."
+    part = ''
+    for block in reversed_blocks(file):
+        for c in reversed(block):
+            if c == '\n' and part:
+                yield part[::-1]
+                part = ''
+            part += c
+     if part: yield part[::-1]
 
+def reversed_blocks(file, blocksize=4096):
+    "Generate blocks of file's contents in reverse order."
+    file.seek(0, os.SEEK_END)
+    here = file.tell()
+    while 0 < here:
+        delta = min(blocksize, here)
+        file.seek(here - delta, os.SEEK_SET)
+        yield file.read(delta)
+        here -= delta
