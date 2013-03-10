52cc79c2fcc9355accc7bc66609317146da2f4bd
Motoz <stokkheim@gmail.com>
2013-03-10 15:37:13 +0200

    added 'debug' option to pellmonsrv, does not daemonize

--- pellmonsrv.py
+++ pellmonsrv.py
@@ -654,6 +654,8 @@ if __name__ == "__main__":
             daemon.stop()
         elif 'restart' == sys.argv[1]:
             daemon.restart()
+        elif 'debug' == sys.argv[1]:
+            daemon.run()
         else:
             print "Unknown command"
             sys.exit(2)
