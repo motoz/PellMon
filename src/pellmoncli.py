1207cf34e72f089b27b50d37282de181b3cb35fc
Motoz <stokkheim@gmail.com>
2013-09-18 15:21:21 +0300

    cli client switches to session bus if system bus fails

--- src/pellmoncli.py
+++ src/pellmoncli.py
@@ -113,10 +113,16 @@ def cli(args):
     
 if __name__ == "__main__":
 
-    # Connect to pellmonsrv on the dbus system bus
-    d = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
-    notify = Gio.DBusProxy.new_sync(d, 0, None, 'org.pellmon.int', '/org/pellmon/int', 'org.pellmon.int', None)
-
+    try:
+        # Connect to pellmonsrv on the dbus system bus
+        d = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
+        notify = Gio.DBusProxy.new_sync(d, 0, None, 'org.pellmon.int', '/org/pellmon/int', 'org.pellmon.int', None)
+        version=notify.getItem('version')
+    except:
+        # if that fails try the session bus instead
+        d = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        notify = Gio.DBusProxy.new_sync(d, 0, None, 'org.pellmon.int', '/org/pellmon/int', 'org.pellmon.int', None)
+    
     # create the top-level parser
     parser = argparse.ArgumentParser(prog='pellmoncli')
 
