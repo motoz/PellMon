1f014b0114fd83be095035fa41736adbc97fc589
Anders Nylund <stokkheim@gmail.com>
2014-03-28 15:03:13 +0200

    experimenting with svg process image updated from javascript

--- src/Pellmonweb/media/js/index.js
+++ src/Pellmonweb/media/js/index.js
@@ -208,3 +208,35 @@ $('#graph').load(function() {
     setGraphTitle();
 });
 
+// fetches the document for the given embedding_element
+function getSubDocument(embedding_element)
+{
+	if (embedding_element.contentDocument) 
+	{
+		return embedding_element.contentDocument;
+	} 
+	else 
+	{
+		var subdoc = null;
+		try {
+			subdoc = embedding_element.getSVGDocument();
+		} catch(e) {}
+		return subdoc;
+	}
+}
+
+function findSVGElements()
+{
+	var elms = document.querySelectorAll(".emb");
+	for (var i = 0; i < elms.length; i++)
+	{
+		var subdoc = getSubDocument(elms[i])
+		if (subdoc)
+//			subdoc.getElementById("svgbar").setAttribute("fill", "lime");
+			var sub2 = subdoc.getElementById("mytextid")
+			sub2.textContent ="sadf";
+	}
+}
+
+window.addEventListener("load", findSVGElements, false);
+
