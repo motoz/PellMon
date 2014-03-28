60df9b1b10bae2ac0c24af19db51d4177015b1f8
Motoz <stokkheim@gmail.com>
2014-03-28 19:23:45 +0200

    added som real parameters to process image

--- src/Pellmonweb/media/js/index.js
+++ src/Pellmonweb/media/js/index.js
@@ -225,7 +225,7 @@ function getSubDocument(embedding_element)
 	}
 }
 
-function findSVGElements()
+function findSVGElements(name, value)
 {
 	var elms = document.querySelectorAll(".emb");
 	for (var i = 0; i < elms.length; i++)
@@ -233,10 +233,45 @@ function findSVGElements()
 		var subdoc = getSubDocument(elms[i])
 		if (subdoc)
 //			subdoc.getElementById("svgbar").setAttribute("fill", "lime");
-			var sub2 = subdoc.getElementById("mytextid")
-			sub2.textContent ="sadf";
+			var sub2 = subdoc.getElementById("paramname_" + name)
+			sub2.textContent = value;
 	}
 }
 
 window.addEventListener("load", findSVGElements, false);
+
+function url(s) {
+    var l = window.location;
+    return ((l.protocol === "https:") ? "wss://" : "ws://") + l.hostname + (((l.port != 80) && (l.port != 443)) ? ":" + l.port : "") + '/websocket' +s;
+}
+
+$(document).ready(function() {
+
+    websocket = url('/ws/'+ 'boiler_temp,smoke_temp,chute_temp,magazine_contents');
+    if (window.WebSocket) {
+        ws = new WebSocket(websocket);
+    }
+    else if (window.MozWebSocket) {
+        ws = MozWebSocket(websocket);
+    }
+    else {
+        console.log('WebSocket Not Supported');
+        return;
+    }
+
+    window.onbeforeunload = function(e) {
+        ws.close();
+    };
+
+    ws.onmessage = function (evt) {
+        jsonObject = $.parseJSON(evt.data);
+        for (i in jsonObject) {
+            obj = jsonObject[i];
+            //container = $('#' + obj.name + '-value');
+            //container.html(obj.value);
+            findSVGElements(obj.name, obj.value);
+     }
+  };
+
+});
 
