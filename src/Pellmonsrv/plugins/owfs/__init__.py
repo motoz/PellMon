d628ebbd8a96f3ae6a5b79e7d4ccea53464ce3ee
motoz <stokkheim@gmail.com>
2015-11-11 21:22:29 +0200

    OWFS works (again) when network is missing on startup, and owfs outputs also fixed These regressions were introduced when pyownet replaced ownet

--- src/Pellmonsrv/plugins/owfs/__init__.py
+++ src/Pellmonsrv/plugins/owfs/__init__.py
@@ -38,7 +38,7 @@ class owfsplugin(protocols):
 
     def activate(self, conf, glob):
         try:
-            import pyownet.protocol
+            self.protocol = __import__('pyownet.protocol').protocol
         except ImportError:
             logger.info('OWFS: python module pyownet is missing')
             raise
@@ -82,12 +82,15 @@ class owfsplugin(protocols):
 
                 self.sensors[self.ow2index[ow_name]] = (owpath, server)
                 if not server in self.proxies:
-                    self.proxies[server] = pyownet.protocol.proxy(host=server, port=port)
-
+                    try:
+                        self.proxies[server] = self.protocol.proxy(host=server, port=port)
+                    except self.protocol.ConnError, e:
+                        self.proxies[server] = (server, port)
+                    
             if ow_data == 'type' and value == 'COUNTER':
                 itemList[self.ow2index[ow_name]]['function'] = 'COUNTER'
                 itemList[self.ow2index[ow_name]]['value'] = 0
-                itemList[self.ow2index[ow_name]]['last_i'] = 0
+                itemList[self.ow2index[ow_name]]['last_i'] = b'0'
                 itemList[self.ow2index[ow_name]]['toggle'] = 0
                 t = Timer(5, self.counter_thread, args=(self.ow2index[ow_name],))
                 t.setDaemon(True)
@@ -104,7 +107,10 @@ class owfsplugin(protocols):
                 owpath = val[0]
                 self.latches[self.ow2index[ow_name]] = (owpath, server)
                 if not server in self.proxies:
-                    self.proxies[server] = pyownet.protocol.proxy(host=server, port=port)
+                    try:
+                        self.proxies[server] = self.protocol.proxy(host=server, port=port)
+                    except self.protocol.ConnError, e:
+                        self.proxies[server] = (server, port)
 
             if ow_data == 'type' and value in ['R','R/W']:
                 itemList[self.ow2index[ow_name]]['type'] = value
@@ -123,12 +129,22 @@ class owfsplugin(protocols):
                     return str(item['value'])
                 else:
                     path, server = self.sensors[self.name2index[itemName]]
-                    proxy = self.proxies[server]
-                    data = proxy.read(path)
-                    item['value'] = data.decode('ascii').strip()
-                    return item['value']
+                    try:
+                        proxy = self.proxies[server]
+                        data = proxy.read(path)
+                        item['value'] = data.decode('ascii').strip()
+                        return item['value']
+                    except AttributeError:
+                        host, port = self.proxies[server]
+                        proxy = self.protocol.proxy(host=host, port=port)
+                        self.proxies[server] = proxy
+                        data = proxy.read(path)
+                        item['value'] = data.decode('ascii').strip()
+                        return item['value']
+                        
             except Exception, e:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
+                print exc_type, exc_value
                 traceback.print_tb(exc_traceback, limit=10, file=sys.stdout)
                 return str(e)
         elif not background_poll:
@@ -147,14 +163,24 @@ class owfsplugin(protocols):
             if itemList[index]['type'] == 'R/W':
                 path, server = self.sensors[self.name2index[itemName]]
                 proxy = self.proxies[server]
-                data = proxy.write(path)
-                return 'OK'
+
+                try:
+                    proxy = self.proxies[server]
+                    proxy.write(path, str(value))
+                    return 'OK'
+                except AttributeError:
+                    host, port = self.proxies[server]
+                    proxy = self.protocol.proxy(host=host, port=port)
+                    self.proxies[server] = proxy
+                    proxy.write(path, str(value))
+                    return 'OK'
             else:
                 return 'error'
         except Exception, e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
+            print exc_type, exc_value
             traceback.print_tb(exc_traceback, limit=10, file=sys.stdout)
-            return str(e)
+            return 'error'
 
     def counter_thread(self, counter):
         while True:
@@ -164,14 +190,24 @@ class owfsplugin(protocols):
                 if self.latches.has_key(counter):
                     path, server = self.latches[counter]
                     proxy = self.proxies[server]
-                    data = proxy.read(path)
-                    proxy.write(path, b'0')
+                    try:
+                        proxy = self.proxies[server]
+                        data = proxy.read(path)
+                    except AttributeError:
+                        host, port = self.proxies[server]
+                        proxy = self.protocol.proxy(host=host, port=port)
+                        self.proxies[server] = proxy
+                        data = proxy.read(path)
+                        
                     l = int(data)
-
+                    if l:
+                        proxy.write(path, b'0')
+ 
                 if (counter in self.latches and l == 1) or (counter not in self.latches):
                     path, server = self.sensors[counter]
                     proxy = self.proxies[server]
-                    i = proxy.read(path).decode('ascii')
+                    i = proxy.read(path)
+        
                     if i == item['last_i']:
                         item['value'] += 1
                         item['toggle'] = 0
@@ -182,6 +218,9 @@ class owfsplugin(protocols):
                             item['value'] +=1
                             item['toggle'] = 0
             except Exception, e:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                print exc_type, exc_value
+                traceback.print_tb(exc_traceback, limit=10, file=sys.stdout)
                 logger.debug('OWFS counter error '+str(e))
             sleep(5)
         
